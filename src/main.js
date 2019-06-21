9d3dcf4271586d8e488188fe556177189fba840c
liufy <liufy0211@163.com>
2019-06-21 23:09:24 +0800

    用户登录-发送验证码-请求获取初始化验证码的参数

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 先找文件，没有就找目录
 // 如果找到目录，优先加载目录中的index
 import router from './router'
+// 引入公共样式文件，最好在element 样式文件之后，可以自定义修改element内置样式
+import './styles/index.less'
 
 Vue.use(ElementUI)
 
